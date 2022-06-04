d80af6a6b462024486f04cd19ff3beb0f00471b0
Janusred <horaciok300@hotmail.com>
2022-06-04 04:00:56 -0500

    templates

--- jwp/settings.py
+++ jwp/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+from distutils import core
 import os
+from tkinter import Y
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',   # <====
 ]
 
 MIDDLEWARE = [
