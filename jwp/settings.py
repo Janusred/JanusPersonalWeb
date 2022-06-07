9e77d5a757de530fb2668282962e48f679da47db
Janusred <horaciok300@hotmail.com>
2022-06-07 14:11:37 -0500

    JanusII

--- jwp/settings.py
+++ jwp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 from distutils import core
 import os
-
+from tkinter import Y
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
